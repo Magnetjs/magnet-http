dc9980cd7ac21f871a8f83cf49930329ef1f8da3
Kieve Chua <kievechua@gmail.com>
2017-03-20 16:37:26 +0800

    Fix build issue

--- flyfile.js
+++ flyfile.js
@@ -10,8 +10,22 @@ export async function build(fly) {
   ]);
 }
 
+export async function buildAndCopy(fly) {
+  await fly.serial([
+    'clean',
+    'compileTypescript',
+    'copyBasic',
+  ]);
+}
+
 export async function clean(fly) {
   await fly.clear('dist');
+}
+
+export async function copyBasic(fly) {
+  await fly.source(['package.json', 'LICENSE', 'README.md'])
+    .flatten({ levels: 5 })
+    .target('dist');
 }
 
 export async function compileTypescript(fly) {
